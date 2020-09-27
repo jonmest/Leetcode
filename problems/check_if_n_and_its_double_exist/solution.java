6aecd86f2f833f8d0a3a3e601f916359787726cd
J. William Cavallie Mester <jc222rz@student.lnu.se>
2020-09-27 09:44:55 +0000

    Sync LeetCode submission - Check If N and Its Double Exist (java)

--- problems/check_if_n_and_its_double_exist/solution.java
+++ problems/check_if_n_and_its_double_exist/solution.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        HashMap<Integer, Integer> seen = new HashMap<Integer, Integer>();
+        HashMap<Integer, Boolean> seen = new HashMap<Integer, Boolean>();
         for (int i = 0; i < arr.length; i++) {
             if (
-                seen.containsKey(arr[i]*2) ||
-                seen.containsKey((int) Math.floor(arr[i]/2)) && (arr[i] % 2) == 0
-            ){
-                return true;
-            }
-            seen.put(arr[i], i);
+                seen.containsKey(arr[i] * 2) ||
+                seen.containsKey((int) Math.floor(arr[i] / 2)) && arr[i] % 2 == 0
+            ) return true;
+            seen.put(arr[i], true);
         }
         return false;
     }
